46c2bae6a2940e3598f0b4c60cec852a57bec938
Aayu <106157824+doraemon7467@users.noreply.github.com>
2024-05-25 12:30:54 +0530

    initial commit

---
+++ Backend/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const expressAsyncHandler = require("express-async-handler")
+const dotenv = require("dotenv").config();
+const cors = require('cors');
+
+const app = express();
+
+const port = 5000;
+
+
+app.use(cors({
+    origin: 'http://localhost:5173'
+  }))
+
+
+app.get("/api/data",expressAsyncHandler( async (req,res) => {
+    const searchTerm = req.query.search;
+    if(!searchTerm){
+        return res.status(400).json({ Response: 'False', Error: 'No search term provided' })
+    }
+    const response = await fetch(`https://www.omdbapi.com/?s=${searchTerm}&apikey=${process.env.API_KEY}`)
+    const data =await response.json();
+    res.status(200).json(data);
+}))
+
+app.listen(port , () => {
+    console.log(`Server is running on http://localhost:${port}`);
+})
---
+++ Frontend/src/App.jsx
@@ -0,0 +1,62 @@
+import { useEffect, useState , useCallback } from 'react'
+import './App.css'
+import { debounce } from 'lodash';
+// import { FaSearch } from "react-icons/fa";
+
+function App() {
+
+  const [search,setSearch] = useState("");
+  const [movies,setMovies] = useState([]);
+
+  const handleChange = async (e) =>{
+    const value = e.target.value;
+    setSearch(value);
+    console.log('handleChange triggered with value:', value); // Add logging statement
+  }
+
+  const API_KEY = "fc552c7e";
+  const debouncedHandleClick = useCallback(
+    debounce(async (value) => {
+      if(!value){
+        setMovies([]);
+        return ;
+      }
+      const res = await fetch(`http://localhost:5000/api/data?search=${value}`)
+      const data =await res.json();
+      if(data.Response === 'True')
+        setMovies(data.Search);
+      else setMovies([]);
+      console.log('API call triggered with search term:', value); // Add logging statement
+    }, 500),
+    [API_KEY]
+  );
+
+  useEffect(() => {
+    debouncedHandleClick(search);
+  }, [search]);
+
+  return (
+    <>
+    <div className="container">
+      <div className='search'>
+        <input type='text' value={search} placeholder='Search Your Favourite Movies...' onChange={handleChange}/>
+        {/* <div><FaSearch className='icon'onClick={handleClick}/></div> */}
+      </div>
+      <div className="cards">
+        {movies.map((movie) => {
+          return (
+              <div key={movie.imdbID} className='card'>
+                <img src={movie.Poster} alt="Image" />
+                <h2>Title: {movie.Title}</h2>
+                <h2>Type: {movie.Type}</h2>
+                <h2>Year: {movie.Year}</h2>
+              </div>
+        )
+        })}
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
